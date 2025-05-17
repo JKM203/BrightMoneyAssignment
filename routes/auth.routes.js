da5398606432876f104d014b1ef5101a46205e24
JKM203 <jaruplakrishna982@gmail.com>
2025-05-18 00:37:13 +0530

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,7 @@ import { register, login, getProfile } from '../controllers/auth.controller.js';
 import { authenticateToken } from '../utils/auth.middleware.js';
 
 const router = express.Router();
+//routing express
 
 router.post('/register', register);
 router.post('/login', login);
